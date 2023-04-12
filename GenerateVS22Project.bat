6731c45dac9b3a4a05aaf1bd285da1010411fcae
Saurav280191 <sauravsikarwar28@hotmail.com>
2023-04-12 21:35:44 +0530

    Modified CMake and Generator batch files to create TS_ENGINE folder outside TS_ENGINE_BatchingDemo

--- GenerateVS22Project.bat
+++ GenerateVS22Project.bat
@@ -1,22 +1,26 @@
 @echo off
- 
+
+REM Build Clone and build TS_ENGINE
+cd ..
 echo Cloning TS_ENGINE...
 git clone https://github.com/Saurav280191/TS_ENGINE.git
-echo TS_ENGINE cloned complete.
 timeout /t 5 >nul
+echo TS_ENGINE cloned complete.
 cd TS_ENGINE
+git pull
 git checkout tags/v1.1
-cd TS_ENGINE
 call GenerateVS22Project.bat
 
+REM Build BatchingDemo
 cd ..
-
+cd TS_ENGINE_BatchingDemo
 REM Generate Solution
 cmake -G "Visual Studio 17 2022" -A=x64 -B=./build/x64/Debug -DCMAKE_ARCHITECTURE=x64 -DCMAKE_BUILD_TYPE=Debug
 REM Build Binaries
 cmake --build build/x64/Debug --config Debug
 
-copy "TS_ENGINE\Dependencies\include\assimp\build\x64\bin\Debug\assimp-vc143-mtd.dll" "build\x64\Debug\bin"
+REM Post build commands
+copy "..\TS_ENGINE\Dependencies\include\assimp\build\x64\bin\Debug\assimp-vc143-mtd.dll" "build\x64\Debug\bin"
 
 xcopy "Assets" "build\x64\debug\Assets" /E /I /Y
 xcopy "Assets" "build\x64\debug\bin\Assets" /E /I /Y
